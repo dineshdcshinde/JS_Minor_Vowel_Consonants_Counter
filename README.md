bd36dda2691468abf49da7df9421d5ecbbd89e3a
Dinesh_DC <dineshshinde81563@gmail.com>
2024-11-13 13:32:59 +0530

    Created the feature to count the vowels and the consonants

---
+++ README.md
@@ -0,0 +1,22 @@
+# Vowel and Consonant Counter
+
+## Description
+
+This project is a simple web-based application that counts the number of vowels and consonants in a given text. Users can enter text into a textarea and click a button to see the count of vowels or consonants. This app demonstrates string manipulation techniques in JavaScript and provides a UI for real-time counting.
+
+## Features
+
+- **Vowel Count Button**: Counts and displays the number of vowels in the input text.
+- **Consonant Count Button**: Counts and displays the number of consonants in the input text.
+- **Clear Input and Reset**: After displaying the results, the input and count are reset automatically after 3 seconds.
+- **Error Handling**: Displays a message if no text is entered, prompting the user to enter valid input.
+
+## Installation
+
+To use this project locally, follow these steps:
+
+**Clone the Repository**:
+
+```bash
+git clone https://github.com/dineshdcshinde/JS_Minor_Vowel_Consonants_Counter.git
+```
---
+++ script.js
@@ -0,0 +1,66 @@
+let text = document.querySelector("textarea");
+let vowelBtn = document.querySelector("button");
+let consonantBtn = document.querySelector(".consonants");
+
+// it is to avoid having load of the classes if it is then the output will be same as that class defined
+let output = document.querySelector(".output");
+
+let count = 0;
+
+vowelBtn.addEventListener("click", () => {
+  let vowels = "aeiou";
+
+  let alphabet = text.value.toLowerCase();
+
+  output.classList.remove("success", "failure");
+
+  if (!alphabet) {
+    output.classList.add("failure");
+    output.innerText = "Please enter a text...";
+    return;
+  }
+
+  for (let i = 0; i < alphabet.length; i++) {
+    if (vowels.includes(alphabet[i])) {
+      count++;
+    }
+    output.classList.add("success");
+    output.textContent = `Vowels count ${count}`;
+
+    setTimeout(() => {
+      text.value = "";
+      count = 0;
+      output.textContent = `Vowels count 0`;
+    }, 3000);
+  }
+});
+
+let consonant = 0;
+
+consonantBtn.addEventListener("click", () => {
+  let vowels = "aeiou";
+
+  let alphabet = text.value.toLowerCase();
+
+  output.classList.remove("success", "failure");
+
+  if (!alphabet) {
+    output.classList.add("failure");
+    output.innerText = "Please enter a text...";
+    return;
+  }
+  for (let i = 0; i < alphabet.length; i++) {
+    if (!vowels.includes(alphabet[i]) && alphabet[i].match(/[a-z]i/)) {
+      consonant++;
+    }
+
+    output.classList.add("success");
+    output.textContent = `Consonants count ${consonant}`;
+
+    setTimeout(() => {
+      text.value = "";
+      consonant = 0;
+      output.textContent = `Consonants count 0`;
+    }, 3000);
+  }
+});
